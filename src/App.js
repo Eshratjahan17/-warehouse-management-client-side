3a0befa3fb58cef722edc8c752cc588c0fa6dbf6
Eshratjahan17 <eshratjahan701@gmail.com>
2022-05-15 10:02:38 +0600

    basic installation and setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>welcome to react after so long eshrat,don't worry u'll definately make it</h1>
+    </div>
+  );
+}
+
+export default App;
