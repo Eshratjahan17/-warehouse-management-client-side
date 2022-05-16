95d87e89576524eadb0a066666e1ed762332274d
Eshratjahan17 <eshratjahan701@gmail.com>
2022-05-17 03:41:39 +0600

    data fetch and post

--- src/pages/AddItems/AddItems.js
+++ src/pages/AddItems/AddItems.js
@@ -2,34 +2,66 @@ import React from 'react';
 import { Button, Form } from 'react-bootstrap';
 
 const AddItems = () => {
+  const handlePost=(event)=>{
+    event.preventDefault();
+    const productName = event.target.productName.value;
+    const supplierName = event.target.supplierName.value;
+    const about = event.target.about.value;
+    const price = event.target.price.value;
+    const quantity = event.target.quantity.value;
+    const picture = event.target.picture.value;
+   const productDetail= {productName, supplierName, about, price, quantity, picture};
+   fetch("http://localhost:5000/product", {
+     method: "POST",
+     headers: {
+       "Content-type": "application/json; charset=UTF-8",
+     },
+     body:JSON.stringify(productDetail)
+   })
+   .then(res=>res.json())
+   .then(data =>console.log(data));
+
+  }
   return (
     <div>
       <h1>Add item Here!!</h1>
-      <Form className="mx-auto w-50 signup-form">
+      <Form onSubmit={handlePost} className="mx-auto w-50 signup-form">
         <Form.Group className="mb-3">
           <Form.Label>Product Name</Form.Label>
-          <Form.Control type="text" placeholder="Enter Product Name" />
+          <Form.Control
+            type="text"
+            name="productName"
+            placeholder="Enter Product Name"
+          />
         </Form.Group>
 
         <Form.Group className="mb-3">
           <Form.Label>Supplier Name</Form.Label>
-          <Form.Control type="text" placeholder="Supplier Name" />
+          <Form.Control
+            type="text"
+            name="supplierName"
+            placeholder="Supplier Name"
+          />
         </Form.Group>
         <Form.Group className="mb-3">
           <Form.Label>About product</Form.Label>
-          <Form.Control type="text" placeholder="About" />
+          <Form.Control type="text" name="about" placeholder="About" />
         </Form.Group>
         <Form.Group className="mb-3">
           <Form.Label>Price</Form.Label>
-          <Form.Control type="number" placeholder="Price" />
+          <Form.Control type="number" name="price" placeholder="Price" />
         </Form.Group>
         <Form.Group className="mb-3">
           <Form.Label>Sold quantity</Form.Label>
-          <Form.Control type="number" placeholder="sold quantity" />
+          <Form.Control
+            type="number"
+            name="quantity"
+            placeholder="sold quantity"
+          />
         </Form.Group>
         <Form.Group className="mb-3">
           <Form.Label>Image Url</Form.Label>
-          <Form.Control type="url" placeholder="Image url" />
+          <Form.Control type="url" name="picture" placeholder="Image url" />
         </Form.Group>
 
         <Button variant="success" type="submit">
